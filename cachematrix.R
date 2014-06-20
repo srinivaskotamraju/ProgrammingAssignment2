cc30ff8b66d5246a8cd94831039bd580e1502dbe
srinivaskotamraju <kotamraju.srinivas@gmail.com>
2014-06-20 16:47:47 +0530

    Initial commit. implementation of makeCacheMatrix and solveMatrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+# R File which has two functions 
+# makeCacheMatrix
+# cacheSolve
 
-## Write a short comment describing this function
+# Function which has a matrix and its inverse. Methods are exposed to
+# get, set matrix and also its inverse (getInverse,setInverse).
+# When the matrix is modified (through set method), it is also ensured that its
+# inverse is invalidated.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(lMat= matrix()) {
+        mInverse <- NULL
 
+		# When matrix is changed (which can only happen with set method)
+		# the inverse also initialized to NULL.
+		# mInverse is NULL, signifies, that inverse is not calculated (yet) or 
+		# matrix could have been changed. Checking for minverse == NULL would satisfies both conditions.
+		# Other options that can be used to check if matrix has changed are
+		# multiply the matrix with its inverse and see if it generates identity, if the matrix
+		# is changed, identity would not be generated, but this is little costly than the above check.
+		
+		# Set method to change matrix, when matrix is changed we invalidate the inverse also.
+		
+        set <- function(y) {
+                lMat <<- y
+                mInverse  <<- NULL
+        }
+
+        get <- function() lMat
+		
+		#Methods to set and get inverse.
+		
+        setInverse <- function(lInverse) mInverse  <<- lInverse
+        getInverse <- function() mInverse 
+
+        list(set = set, get = get,
+             getInverse = getInverse ,
+             setInverse = setInverse)
 }
 
+# Method which calculates inverse of a matrix. 
+# It checks if the inverse is NULL, only then calculates the inverse.
+# If the inverse is NOT NULL, the same is returned.
 
-## Write a short comment describing this function
+cacheSolve <- function(matObj, ...) {
+		mInverse <- matObj$getInverse()
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+		#Check to see if the inverse is NOT NULL and if yes return the same.
+		
+		if(!is.null(mInverse)) {
+			message("getting cached inverse")
+			
+			#Return the calculated inverse.
+			return(mInverse)	
+		}
+		#Calculate the inverse (assumption the matrix is always invertible).
+		
+		mInverse<-solve(matObj$get())
+		
+		#Store the calculated inverse for subsequent calls (caching).
+		matObj$setInverse(mInverse)
+		
+		#Return the calculated inverse.
+		mInverse
 }
