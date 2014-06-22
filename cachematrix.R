150ea2e56c609c95b775d5b63162dc97d254dac2
srinivaskotamraju <kotamraju.srinivas@gmail.com>
2014-06-22 21:20:13 +0530

    Few more comments added. Local variable renamed,changed.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 
 # Function which has a matrix and its inverse. Methods are exposed to
 # get, set matrix and also its inverse (getInverse,setInverse).
-# When the matrix is modified (through set method), it is also ensured that its
+# When the matrix is modified (using the set method), it is also ensured that its
 # inverse is invalidated.
 
 makeCacheMatrix <- function(lMat= matrix()) {
@@ -17,8 +17,10 @@ makeCacheMatrix <- function(lMat= matrix()) {
 		# Other options that can be used to check if matrix has changed are
 		# multiply the matrix with its inverse and see if it generates identity, if the matrix
 		# is changed, identity would not be generated, but this is little costly than the above check.
+		# We can also use all.equal to see if the matrix is changed, and only if it is changed then
+		# update the matrix and invalidate the inverse.
 		
-		# Set method to change matrix, when matrix is changed we invalidate the inverse also.
+		# Set method to change,update matrix, when matrix is changed we also invalidate the inverse.
 		
         set <- function(y) {
                 lMat <<- y
@@ -41,8 +43,8 @@ makeCacheMatrix <- function(lMat= matrix()) {
 # It checks if the inverse is NULL, only then calculates the inverse.
 # If the inverse is NOT NULL, the same is returned.
 
-cacheSolve <- function(matObj, ...) {
-		mInverse <- matObj$getInverse()
+cacheSolve <- function(lMatObj, ...) {
+		mInverse <- lMatObj$getInverse()
 
 		#Check to see if the inverse is NOT NULL and if yes return the same.
 		
@@ -52,12 +54,12 @@ cacheSolve <- function(matObj, ...) {
 			#Return the calculated inverse.
 			return(mInverse)	
 		}
-		#Calculate the inverse (assumption the matrix is always invertible).
 		
-		mInverse<-solve(matObj$get())
+		#Calculate the inverse (assumption that matrix is always invertible).
+		mInverse<-solve(lMatObj$get())
 		
 		#Store the calculated inverse for subsequent calls (caching).
-		matObj$setInverse(mInverse)
+		lMatObj$setInverse(mInverse)
 		
 		#Return the calculated inverse.
 		mInverse
